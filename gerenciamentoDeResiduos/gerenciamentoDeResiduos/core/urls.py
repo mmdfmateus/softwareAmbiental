acb619911d2e215c12d6d02b3d600e2b93269e0b
Mateus Mourao <mmdfmateus@gmail.com>
2017-11-24 02:37:35 -0200

    deletado urls do core

--- gerenciamentoDeResiduos/gerenciamentoDeResiduos/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-from gerenciamentoDeResiduos.core import views
-
-urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^contato/$', views.contact, name='contact'),
-]
--- gerenciamentoDeResiduos/gerenciamentoDeResiduos/urls.py
+++ gerenciamentoDeResiduos/gerenciamentoDeResiduos/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-from .urls import *
+from .urls import
 from gerenciamentoDeResiduos.core import views
 
 urlpatterns = [
